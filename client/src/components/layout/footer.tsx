55036601deaa4c8140b6fc3eb541db2b735e771c
Siphesihle00001101 <sdhlamini@i1solutions.co.za>
2025-06-05 11:05:53 +0200

    Updated contact number

--- client/src/components/layout/footer.tsx
+++ client/src/components/layout/footer.tsx
@@ -48,7 +48,7 @@ export default function Footer() {
             <h3 className="font-semibold text-white mb-4">Contact</h3>
             <ul className="space-y-2 text-sm">
               <li>pmbsecurityservices@gmail.com</li>
-              <li>+27 769 7605</li>
+              <li>060 769 7605</li>
               <li>37 Natal Street</li>
               <li>Bellevue East</li>
               <li>Johannesburg, Gauteng 2198</li>
